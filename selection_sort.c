39eb3e7eee5fde54f428e39fefeea3e46d88bde1
advaitha jayakumar <advaithajayakumar@gmail.com>
2021-04-19 10:32:46 +0530

    completed the selection sort procedures

--- selection_sort.c
+++ selection_sort.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 int main(){
-    int arr[50],size,i;
+    int arr[50],size,i,j,small,temp;
     printf("ener the size of array");
     scanf("%d",&size);
     for(i=0;i<size;++i){
         printf("%d element of tha array",(i+1));
         scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size;++i){
+        small=i;
+        for(j=i+1;j<size;++j){
+            if(arr[small]>arr[j]){
+                small=j;
+            }
+        }
+        temp=arr[i];
+        arr[i]=arr[small];
+        arr[small]=arr[i];
     }
     return 0;
 }
